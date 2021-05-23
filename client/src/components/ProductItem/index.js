9733efd2802346d8532d5ff1f622938d160d03af
jpkashlak <jpkashlak@gmail.com>
2021-05-23 11:54:06 -0500

    adjusted ProductItem for Redux

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from '../../utils/GlobalState';
+// import { useStoreContext } from '../../utils/GlobalState';
+import { useDispatch, useSelector } from 'react-redux';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 import CartItem from "../CartItem";
 import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
+  // const [state, dispatch] = useStoreContext();
+  const state = useSelector((state) => {
+    return state
+  });
+  const dispatch = useDispatch();
+  
   const { cart } = state;
   const {
     image,
